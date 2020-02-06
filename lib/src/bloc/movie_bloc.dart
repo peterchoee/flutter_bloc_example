c94d3ac5ddaa68058afd43f6cfe3470e409cc53e
choedeb <choedeb@gmail.com>
2020-02-06 23:28:58 +0900

    movie result bloc 구현

--- lib/src/bloc/movie_bloc.dart
+++ lib/src/bloc/movie_bloc.dart
@@ -1,3 +1,21 @@
-class MovieBloc {
+import 'package:flutter_bloc_example/src/model/movie_result.dart';
+import 'package:flutter_bloc_example/src/repository/repository.dart';
+import 'package:rxdart/rxdart.dart';
 
+class MovieBloc {
+  final _repository = Repository();
+  final _moviesFetcher = PublishSubject<MovieResult>();
+
+  Stream<MovieResult> get allMovies => _moviesFetcher.stream;
+
+  fetchAllMovies() async {
+    MovieResult itemModel = await _repository.fetchRankMovie();
+    _moviesFetcher.sink.add(itemModel);
+  }
+
+  dispose() {
+    _moviesFetcher.close();
+  }
 }
+
+final bloc = MovieBloc();
